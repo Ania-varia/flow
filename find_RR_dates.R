7026e27b31e759973f0c5f46f82940fa638b9cd0
Ania Lorenc <29759130+Ania-varia@users.noreply.github.com>
2018-08-07 16:59:16 +0100

    small comments

--- find_RR_dates.R
+++ find_RR_dates.R
@@ -5,21 +5,26 @@ find_RR_dates <- function(whole_dt, param, by_sex=FALSE, min_ind=70){
   #min_ind - how many controls individuals
   #param - on which parameter to base control finding
   if(!is.data.table(whole_dt)){
+    #making sure the input is data table
     whole_dt <-data.table(whole_dt)
   }
   if(by_sex){
+    #when splitting by sex, simply run for each subset and concatenate output
     all_res <- rbind(find_RR_dates(whole_dt[Gender=="Female"], param, by_sex=FALSE)[,sex:="female"],
                      find_RR_dates(whole_dt[Gender=="Male"], param, by_sex=FALSE)[,sex:="male"])
     return(all_res)
   }
   
   test_BM <- whole_dt[!is.na(param),c("Assay_Date", "Gender", param, "Genotype"), with=F]
+  test_BM <- test_BM [order(Assay_Date)] 
   test_BM_WT <- take_only_WTs(test_BM)
   test_BM_WT[,wt_date:=Assay_Date]
   test_BM_WT  <-test_BM_WT[,.(.N), by=.(Assay_Date,wt_date)]
+  setkey(test_BM_WT, Assay_Date)
   test_BM_WT[, cumsum_fromstart:=cumsum(N)]
   test_BM_WT[, cumsum_fromend:=rev(cumsum(rev(N)))]
-  setkey(test_BM_WT, Assay_Date)
+  
+  #finding indices - this is max and min because of some history, feel free to make it faster with first/last element approach
   #find start indices
   st <- apply(test_BM_WT,1, function(x) {max(which(as.numeric(x["cumsum_fromstart"])- as.numeric(test_BM_WT$cumsum_fromstart) - as.numeric(x["N"])>=ceiling((min_ind-as.numeric(x["N"]))/2)) )})
   st[st<0] <-1
@@ -27,17 +32,18 @@ find_RR_dates <- function(whole_dt, param, by_sex=FALSE, min_ind=70){
   end <-apply(test_BM_WT,1, function(x) {min(which(as.numeric(test_BM_WT$cumsum_fromstart) - as.numeric(x["cumsum_fromstart"])>=ceiling((min_ind-as.numeric(x["N"]))/2))) })
   end[end>length(end)] <-length(end)
   test_BM_WT <-cbind(test_BM_WT, st,end)
-  #fill start and end dates for days with enough samples before/after
+  
+  #fill start and end dates for days with enough samples available before/after (better too many than too few, hence ceiling)
   test_BM_WT[cumsum_fromstart-N >= ceiling((min_ind-N)/2) & cumsum_fromend-N >= ceiling((min_ind-N)/2)
              ,st_date:=test_BM_WT$Assay_Date[st]]
   test_BM_WT[cumsum_fromstart-N>=ceiling((min_ind-N)/2) & cumsum_fromend-N >= ceiling((min_ind-N)/2)
              ,end_date:=test_BM_WT$Assay_Date[end]]
   
-  #fill start and end dates for days at the beginning/end
-  test_BM_WT[cumsum_fromstart-N < ceiling((min_ind-N)/2),st_date:=test_BM_WT$Assay_Date[1]]
-  test_BM_WT[cumsum_fromstart-N < ceiling((min_ind-N)/2), end_date:=test_BM_WT[min(which(cumsum_fromstart>=min_ind)),Assay_Date]]
+  #fill start and end dates for days at the beginning/end (without enough  samples available before/after)
+  test_BM_WT[cumsum_fromstart-N < ceiling((min_ind-N)/2), st_date:=test_BM_WT$Assay_Date[1]]
+  test_BM_WT[cumsum_fromstart-N < ceiling((min_ind-N)/2), end_date:=test_BM_WT[min(which(cumsum_fromstart>=min_ind)), Assay_Date]]
   
-  test_BM_WT[cumsum_fromend-N < ceiling((min_ind-N)/2), st_date:=test_BM_WT[max(which(cumsum_fromend>=min_ind)),Assay_Date]]
+  test_BM_WT[cumsum_fromend-N < ceiling((min_ind-N)/2), st_date:=test_BM_WT[max(which(cumsum_fromend>=min_ind)), Assay_Date]]
   test_BM_WT[cumsum_fromend-N < ceiling((min_ind-N)/2), end_date:=max(Assay_Date)]
   
   #Fix days when there are no WTs
